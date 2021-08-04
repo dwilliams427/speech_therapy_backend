ed539b326b06529f20ff23435cd76e555a8cd5b8
dwilliams427 <dlw1ll27@gmail.com>
2021-08-04 10:31:35 -0700

    seeds db, adds user authentication, adds sessions, adds bcrypt and jwt gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create({ username: "daniel", email: "dw@example.com", password: "password" })
+
+water = Card.create({name: "water", image: "image", sound: "bloop bloop"})
+bathroom = Card.create({name: "bathroom", image: "image", sound: "woosh"})
+kitchen = Card.create({name: "kitchen", image: "image", sound: "kitchen sounds"}) 
+hungry = Card.create({name: "hungry", image: "image", sound: "hungry sounds"})
+
+food = Tag.create({name: "food"})
+water = Tag.create({name: "water"})
+toilet = Tag.create({name: "toilet"})
+pain = Tag.create({name: "pain"})
+
